8655b4d4c959e3583a0972e0bb65e013b7059caf
José Luis Ramírez Morales <luis.ramirez@parhikuni.com>
2020-10-09 15:33:31 -0500

    respuestas rápidas

--- app.js
+++ app.js
@@ -44,7 +44,19 @@ function defaultMessage(senderId) {
       id: senderId,
     },
     message: {
-      text: 'Hola soy un bot de messenger y te invito a utilizar nuestro menú',
+      text: 'Hola soy un bot de messenger y te invito a utilizar nuestro menú',,
+      "quick_replies": [
+        {
+          "content_type": "text",
+          "title": "¿Quieres una Pizza?",
+          "payload": "PIZZAS_PAYLOAD"
+        },
+        {
+          "content_type": "text",
+          "title": "Acerca de",
+          "payload": "ABOUT_PAYLOAD"
+        },
+      ]
     },
   };
   callSendApi(messageData);
